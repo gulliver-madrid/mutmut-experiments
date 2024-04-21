5e85df039e41ed909b54af688819b7b03fe5bc22
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-21 21:07:06 +0200

    move Config to its own module

--- mutmut/__init__.py
+++ mutmut/__init__.py
@@ -34,6 +34,7 @@ from parso import parse
 from parso.python.tree import Name, Number, Keyword, FStringStart, FStringEnd, Module
 from parso.tree import Node, BaseNode, Leaf
 
+from mutmut.config import Config
 from mutmut.utils import status_printer
 
 __version__ = '2.4.5'
@@ -829,33 +830,6 @@ def run_mutation(context: Context, callback) -> str:
             result = subprocess.check_output(config.post_mutation, shell=True).decode().strip()
             if result and not config.swallow_output:
                 callback(result)
-
-
-@dataclass
-class Config:
-    swallow_output: bool
-    test_command: str
-    _default_test_command: str = field(init=False)
-    covered_lines_by_filename: Optional[Dict[str, set[Optional[int]]]]
-    baseline_time_elapsed: float
-    test_time_multiplier: float
-    test_time_base: float
-    dict_synonyms: List[str]
-    total: int
-    using_testmon: bool
-    tests_dirs: List[str]
-    hash_of_tests: str
-    post_mutation: str
-    pre_mutation: str
-    coverage_data: Dict[str, Dict[int, List[str]]]
-    paths_to_mutate: List[str]
-    mutation_types_to_apply: Set[str]
-    no_progress: bool
-    ci: bool
-    rerun_all: bool
-
-    def __post_init__(self):
-        self._default_test_command = self.test_command
 
 
 def tests_pass(config: Config, callback) -> bool:
---
+++ mutmut/config.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional, Set
+
+
+@dataclass
+class Config:
+    swallow_output: bool
+    test_command: str
+    _default_test_command: str = field(init=False)
+    covered_lines_by_filename: Optional[Dict[str, set[Optional[int]]]]
+    baseline_time_elapsed: float
+    test_time_multiplier: float
+    test_time_base: float
+    dict_synonyms: List[str]
+    total: int
+    using_testmon: bool
+    tests_dirs: List[str]
+    hash_of_tests: str
+    post_mutation: str
+    pre_mutation: str
+    coverage_data: Dict[str, Dict[int, List[str]]]
+    paths_to_mutate: List[str]
+    mutation_types_to_apply: Set[str]
+    no_progress: bool
+    ci: bool
+    rerun_all: bool
+
+    def __post_init__(self):
+        self._default_test_command = self.test_command
