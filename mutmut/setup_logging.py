b1b5925f2d5598ac637f0673365543eb3a45b27b
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-04-21 22:39:57 +0200

    add setup_logging.py

---
+++ mutmut/setup_logging.py
@@ -0,0 +1,40 @@
+import logging
+from pathlib import Path
+from pprint import pformat
+
+
+def get_main_directory() -> Path:
+    return Path(__file__).parents[1]
+
+
+# Funcion para configurar un logger y enviar su salida a un archivo
+def configure_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
+    log_file_name = f"{name}.log"
+    # Crea un logger especifico
+    logger = logging.getLogger(name)
+    logger.propagate = False  # Evita la propagacion al logger raiz
+    logger.setLevel(level)  # Establece el nivel del logger
+
+    # Crea un FileHandler especifico para escribir en un archivo
+    path = get_main_directory() / "logs"
+    if not path.exists():
+        path.mkdir()
+    file_handler = logging.FileHandler(path / log_file_name, encoding="utf-8")
+    file_handler.setLevel(level)  # Establece el nivel del FileHandler
+
+    # Crea un formateador y anadelo al FileHandler
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    file_handler.setFormatter(formatter)
+
+    # Anade el FileHandler al logger
+    logger.addHandler(file_handler)
+    logger.info("\n")
+    logger.info("START")
+
+    return logger
+
+
+def format_var(name: str, obj: object) -> str:
+    return name + "=" + pformat(obj, width=120)
