bd330fe77c498d994371393d604f5b3eff80f16f
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-05-01 15:30:14 +0200

    create a wrapper around parso.parse

---
+++ mutmut/parse.py
@@ -0,0 +1,12 @@
+from typing import Any
+import parso
+
+
+def parse(code: str, **kwargs: Any) -> Any:
+    """
+    A wrapper for parso.parse.
+    Params are documented in :py:meth:`parso.Grammar.parse`.
+
+    :param str version: The version used by :py:func:`parso.load_grammar`.
+    """
+    return parso.parse(code, **kwargs)  # pyright: ignore [reportUnknownVariableType,reportUnknownMemberType]
