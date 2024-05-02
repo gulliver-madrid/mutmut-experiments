be006a9f834dc9325a0e6c870ada9e3724822bf1
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-05-02 10:24:15 +0200

    simplify should_exclude()

--- mutmut/context.py
+++ mutmut/context.py
@@ -69,18 +69,13 @@ class Context:
 
         try:
             covered_lines = list(config.covered_lines_by_filename[self.filename] or set())
-            # covered_lines is a set
         except KeyError:
-            if config.coverage_data is not None:
-                covered_lines_as_dict = config.coverage_data.get(os.path.abspath(self.filename))
-                if covered_lines_as_dict is None:
-                    covered_lines = []
-                else:
-                    covered_lines = list(covered_lines_as_dict.keys())
-                # covered_lines is a dict
-                config.covered_lines_by_filename[self.filename] = covered_lines
-            else:
+            if config.coverage_data is None:
                 covered_lines = []
+            else:
+                covered_lines_as_dict = config.coverage_data.get(os.path.abspath(self.filename), {})
+                covered_lines = list(covered_lines_as_dict.keys())
+                config.covered_lines_by_filename[self.filename] = covered_lines
 
         if not covered_lines:
             return True
