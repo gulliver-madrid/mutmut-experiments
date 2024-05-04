aab64686f40c056d085a2be47d2f23b2d6d20ed3
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-05-04 11:45:08 +0200

    use list always for lines in covered_lines_by_filename

--- mutmut/patch.py
+++ mutmut/patch.py
@@ -1,24 +1,9 @@
 import os
 from pathlib import Path
-from typing import Dict, Mapping, TypeGuard, cast, TYPE_CHECKING
+from typing import Dict, TYPE_CHECKING
 
 
-CoveredLinesByFilename = Dict[str, set[int]]
-
-
-def is_covered_lines_by_filename(obj: object) -> TypeGuard[CoveredLinesByFilename]:
-    if not isinstance(obj, dict):
-        return False
-    d = cast(Mapping[object, object], obj)
-    if not all(isinstance(k, str) for k in d.keys()):
-        return False
-    for v in d.values():
-        if not isinstance(v, set):
-            return False
-        covered_lines = cast(frozenset[object], v)
-        if not all(isinstance(item, int) for item in covered_lines):
-            return False
-    return True
+CoveredLinesByFilename = Dict[str, list[int]]
 
 
 if TYPE_CHECKING:
@@ -41,8 +26,7 @@ def read_patch_data(patch_file_path: str | Path) -> CoveredLinesByFilename:
         diffs = whatthepatch.parse_patch(f.read())
 
     result = {
-        os.path.normpath(get_new_path(diff)): {change.new for change in diff.changes if change.old is None}
+        os.path.normpath(get_new_path(diff)): sorted({change.new for change in diff.changes if change.old is None and change.new is not None})
         for diff in diffs if diff.changes
     }
-    assert is_covered_lines_by_filename(result)
     return result
