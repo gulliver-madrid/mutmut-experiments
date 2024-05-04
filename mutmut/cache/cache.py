0be4fb0153493b37b2e9b3e7f108f63b61888690
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-05-04 15:09:32 +0200

    extract junitxml.py

--- mutmut/cache/cache.py
+++ mutmut/cache/cache.py
@@ -10,7 +10,6 @@ from itertools import groupby, zip_longest
 from types import NoneType
 from typing import TYPE_CHECKING, Any, Callable, ContextManager, Dict, Iterator, List, Sequence, Tuple, TypeVar
 
-from junit_xml import TestSuite, TestCase, to_xml_report_string  # type: ignore [import-untyped]
 from pony.orm import select, RowNotFound, ERDiagramError, OperationalError
 from typing_extensions import ParamSpec
 
@@ -209,39 +208,6 @@ def get_unified_diff_from_filename_and_mutation_id(source: str | None, filename:
     ):
         output += line + "\n"
     return output
-
-
-def print_result_cache_junitxml(dict_synonyms: list[str], suspicious_policy: str, untested_policy: str) -> None:
-    print(create_junitxml_report(dict_synonyms, suspicious_policy, untested_policy))
-
-
-@init_db
-@db_session
-def create_junitxml_report(dict_synonyms: list[str], suspicious_policy: str, untested_policy: str) -> str:
-    test_cases: list[TestCase] = []
-    mutant_list = list(select(x for x in get_mutants()))
-    for filename, mutants in groupby(mutant_list, key=lambda x: x.line.sourcefile.filename):
-        for mutant in mutants:
-            tc = TestCase("Mutant #{}".format(mutant.id), file=filename, line=mutant.line.line_number + 1, stdout=mutant.line.line)
-            if mutant.status == BAD_SURVIVED:
-                tc.add_failure_info(message=mutant.status, output=get_unified_diff(mutant.id, dict_synonyms))
-            if mutant.status == BAD_TIMEOUT:
-                tc.add_error_info(message=mutant.status, error_type="timeout", output=get_unified_diff(mutant.id, dict_synonyms))
-            if mutant.status == OK_SUSPICIOUS:
-                if suspicious_policy != 'ignore':
-                    func = getattr(tc, 'add_{}_info'.format(suspicious_policy))
-                    func(message=mutant.status, output=get_unified_diff(mutant.id, dict_synonyms))
-            if mutant.status == UNTESTED:
-                if untested_policy != 'ignore':
-                    func = getattr(tc, 'add_{}_info'.format(untested_policy))
-                    func(message=mutant.status, output=get_unified_diff(mutant.id, dict_synonyms))
-
-            test_cases.append(tc)
-
-    ts = TestSuite("mutmut", test_cases)
-    report: Any = to_xml_report_string([ts])
-    assert isinstance(report, str)
-    return report
 
 
 def sequence_ops(a: list[str], b: list[str]) -> Iterator[tuple[str, str, int | None, str | None, int | None]]:
