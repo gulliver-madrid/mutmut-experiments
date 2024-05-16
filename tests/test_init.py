58569ebdac4ad0f508a84596a7541e874eea3937
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-05-16 14:53:06 +0200

    add Any type to annotations in several functions

--- tests/test_init.py
+++ tests/test_init.py
@@ -40,8 +40,8 @@ def test_context_exclude_line() -> None:
     assert mutate(Context(source=source)) == (source, 0)
 
 
-def check_mutants_stub(**kwargs) -> None:
-    def run_mutation_stub(*_) -> Literal['ok_killed']:
+def check_mutants_stub(**kwargs: Any) -> None:
+    def run_mutation_stub(*_: Any) -> Literal['ok_killed']:
         sleep(0.15)
         return OK_KILLED
     check_mutants_original = check_mutants
@@ -67,7 +67,7 @@ def test_run_mutation_tests_thread_synchronization(monkeypatch: Any) -> None:
         kwargs['mutants_queue'].put(('end', None))
     monkeypatch.setattr('mutmut.queue_mutants', queue_mutants_stub)
 
-    def update_mutant_status_stub(**_) -> None:
+    def update_mutant_status_stub(**_: Any) -> None:
         sleep(0.1)
 
     monkeypatch.setattr('mutmut.check_mutants', check_mutants_stub)
@@ -77,7 +77,7 @@ def test_run_mutation_tests_thread_synchronization(monkeypatch: Any) -> None:
     progress_mock = MagicMock()
     progress_mock.registered_mutants = 0
 
-    def progress_mock_register(*_):
+    def progress_mock_register(*_: Any) -> None:
         progress_mock.registered_mutants += 1
 
     progress_mock.register = progress_mock_register
