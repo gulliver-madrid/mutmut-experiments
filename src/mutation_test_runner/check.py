c9fcf174a33cd5fad347f274d82c8d02ed232ced
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-05-30 15:38:47 +0200

    move down private functions in mutation_test_runner\check.py

--- src/mutation_test_runner/check.py
+++ src/mutation_test_runner/check.py
@@ -67,89 +67,6 @@ def mutate_file(backup: bool, context: Context) -> Tuple[str, str]:
     return original, mutated
 
 
-def tests_pass(config: Config, callback: StrConsumer) -> bool:
-    """
-    :return: :obj:`True` if the tests pass, otherwise :obj:`False`
-    """
-    if config.using_testmon:
-        copy(".testmondata-initial", ".testmondata")
-
-    use_special_case = True
-
-    # Special case for hammett! We can do in-process test running which is much faster
-    if use_special_case and config.test_command.startswith(hammett_prefix):
-        return hammett_tests_pass(config, callback)
-
-    returncode = popen_streaming_output(
-        config.test_command, callback, timeout=config.baseline_time_elapsed * 10
-    )
-    return returncode != 1
-
-
-def hammett_tests_pass(config: Config, callback: StrConsumer) -> bool:
-    # noinspection PyUnresolvedReferences
-    from hammett import main_cli  # type: ignore [import-untyped]
-
-    modules_before = set(sys.modules.keys())
-
-    # set up timeout
-    import _thread
-    from threading import (
-        Timer,
-        current_thread,
-        main_thread,
-    )
-
-    timed_out = False
-
-    def timeout() -> None:
-        _thread.interrupt_main()
-        nonlocal timed_out
-        timed_out = True
-
-    assert current_thread() is main_thread()
-    timer = Timer(config.baseline_time_elapsed * 10, timeout)
-    timer.daemon = True
-    timer.start()
-
-    # Run tests
-    try:
-
-        class StdOutRedirect(TextIOBase):
-            def write(self, s: str) -> int:
-                callback(s)
-                return len(s)
-
-        redirect = StdOutRedirect()
-        sys.stdout = redirect  # type: ignore [assignment]
-        sys.stderr = redirect  # type: ignore [assignment]
-        returncode = main_cli(shlex.split(config.test_command[len(hammett_prefix) :]))
-        sys.stdout = sys.__stdout__
-        sys.stderr = sys.__stderr__
-        timer.cancel()
-    except KeyboardInterrupt:
-        timer.cancel()
-        if timed_out:
-            raise TimeoutError("In process tests timed out")
-        raise
-
-    modules_to_force_unload = {
-        x.partition(os.sep)[0].replace(".py", "") for x in config.paths_to_mutate
-    }
-
-    for module_name in sorted(
-        set(sys.modules.keys()) - set(modules_before), reverse=True
-    ):
-        if (
-            any(module_name.startswith(x) for x in modules_to_force_unload)
-            or module_name.startswith("tests")
-            or module_name.startswith("django")
-        ):
-            del sys.modules[module_name]
-
-    return bool(returncode == 0)
-
-
 def check_mutants(
     mutants_queue: MutantQueue,
     results_queue: ResultQueue,
@@ -233,7 +150,7 @@ def run_mutation(
         mutate_file(backup=True, context=context)
         start = time()
         try:
-            survived = tests_pass(config=config, callback=callback)
+            survived = _tests_pass(config=config, callback=callback)
             if (
                 survived
                 and config.test_command != config.default_test_command
@@ -241,7 +158,7 @@ def run_mutation(
             ):
                 # rerun the whole test suite to be sure the mutant can not be killed by other tests
                 config.test_command = config.default_test_command
-                survived = tests_pass(config=config, callback=callback)
+                survived = _tests_pass(config=config, callback=callback)
         except TimeoutError:
             return BAD_TIMEOUT
 
@@ -275,3 +192,86 @@ def run_mutation(
             )
             if result and not config.swallow_output:
                 callback(result)
+
+
+def _tests_pass(config: Config, callback: StrConsumer) -> bool:
+    """
+    :return: :obj:`True` if the tests pass, otherwise :obj:`False`
+    """
+    if config.using_testmon:
+        copy(".testmondata-initial", ".testmondata")
+
+    use_special_case = True
+
+    # Special case for hammett! We can do in-process test running which is much faster
+    if use_special_case and config.test_command.startswith(hammett_prefix):
+        return _hammett_tests_pass(config, callback)
+
+    returncode = popen_streaming_output(
+        config.test_command, callback, timeout=config.baseline_time_elapsed * 10
+    )
+    return returncode != 1
+
+
+def _hammett_tests_pass(config: Config, callback: StrConsumer) -> bool:
+    # noinspection PyUnresolvedReferences
+    from hammett import main_cli  # type: ignore [import-untyped]
+
+    modules_before = set(sys.modules.keys())
+
+    # set up timeout
+    import _thread
+    from threading import (
+        Timer,
+        current_thread,
+        main_thread,
+    )
+
+    timed_out = False
+
+    def timeout() -> None:
+        _thread.interrupt_main()
+        nonlocal timed_out
+        timed_out = True
+
+    assert current_thread() is main_thread()
+    timer = Timer(config.baseline_time_elapsed * 10, timeout)
+    timer.daemon = True
+    timer.start()
+
+    # Run tests
+    try:
+
+        class StdOutRedirect(TextIOBase):
+            def write(self, s: str) -> int:
+                callback(s)
+                return len(s)
+
+        redirect = StdOutRedirect()
+        sys.stdout = redirect  # type: ignore [assignment]
+        sys.stderr = redirect  # type: ignore [assignment]
+        returncode = main_cli(shlex.split(config.test_command[len(hammett_prefix) :]))
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+        timer.cancel()
+    except KeyboardInterrupt:
+        timer.cancel()
+        if timed_out:
+            raise TimeoutError("In process tests timed out")
+        raise
+
+    modules_to_force_unload = {
+        x.partition(os.sep)[0].replace(".py", "") for x in config.paths_to_mutate
+    }
+
+    for module_name in sorted(
+        set(sys.modules.keys()) - set(modules_before), reverse=True
+    ):
+        if (
+            any(module_name.startswith(x) for x in modules_to_force_unload)
+            or module_name.startswith("tests")
+            or module_name.startswith("django")
+        ):
+            del sys.modules[module_name]
+
+    return bool(returncode == 0)
