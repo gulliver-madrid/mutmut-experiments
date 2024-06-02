7781310e18a20fb244cdcb13f97595c4ec420c7b
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-06-02 13:20:04 +0200

    improve test_parallelize

--- tests/extra_tests/test_parallelize.py
+++ tests/extra_tests/test_parallelize.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import builtins
+import os
 
 from click.testing import CliRunner
 
@@ -11,6 +12,7 @@ from src.__main__ import climain
 
 from helpers import FileSystemPath, open_utf8
 from fixtures_main import (
+    FILE_TO_MUTATE_CONTENTS,
     filesystem,  # pyright: ignore [reportUnusedImport]
 )
 
@@ -18,7 +20,6 @@ builtins.open = open_utf8  # type: ignore [assignment]
 
 
 def test_simple_parallelize(filesystem: FileSystemPath) -> None:
-    print("inicio")
     result = CliRunner().invoke(
         climain,
         ["run", "-s", "--paths-to-mutate=foo.py", "--parallelize"],
@@ -26,5 +27,13 @@ def test_simple_parallelize(filesystem: FileSystemPath) -> None:
     )
     print(repr(result.output))
     assert result.exit_code == 0
-    print(f"{result.exit_code=}")
-    print("fin")
+
+    result = CliRunner().invoke(climain, ["apply", "1"], catch_exceptions=False)
+    print(repr(result.output))
+    assert result.exit_code == 0
+
+    with open(os.path.join(str(filesystem), "foo.py")) as f:
+        # print()
+        # print(f"{f.read()=}")
+        # print(f"{FILE_TO_MUTATE_CONTENTS=}")
+        assert f.read() != FILE_TO_MUTATE_CONTENTS
