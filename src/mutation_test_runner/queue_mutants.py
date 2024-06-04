7a420cd7f8e79b27b389a62baf7daa615dba61f7
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-06-04 15:13:53 +0200

    refactor queue_mutants()
    - extract get_mutants_by_testing_status()

--- src/mutation_test_runner/queue_mutants.py
+++ src/mutation_test_runner/queue_mutants.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from copy import copy as copy_obj
+from dataclasses import dataclass
 from io import open
 from pathlib import Path
-from typing import Mapping, Sequence, TypedDict
+from typing import Iterator, Literal, Mapping, Sequence, TypedDict
 
+from src.cache.cache import get_cached_mutation_statuses
 from src.config import Config
 from src.context import Context, RelativeMutationID
 from src.progress import Progress
@@ -33,36 +35,61 @@ def queue_mutants(
     mutations_by_file: MutationsByFileReadOnly,
     project: Path | None = None,
 ) -> None:
-    from src.cache.cache import get_cached_mutation_statuses
 
     storage.project_path.set_project_path(project)
 
     try:
-        index = 0
-        for filename, mutations in mutations_by_file.items():
-            cached_mutation_statuses = get_cached_mutation_statuses(
-                filename, mutations, config.hash_of_tests
-            )
-            source = get_source(filename)
-            for mutation_id in mutations:
-                cached_status = cached_mutation_statuses.get(mutation_id)
-                assert isinstance(cached_status, str)
-                if is_tested(cached_status):
-                    progress.register(cached_status)
-                    continue
-                context = Context(
-                    mutation_id=mutation_id,
-                    filename=filename,
-                    dict_synonyms=config.dict_synonyms,
-                    config=copy_obj(config),
-                    source=source,
-                    index=index,
-                )
-                mutants_queue.put(("mutant", context))
-                index += 1
+        for mutant in _get_mutants_by_testing_status(mutations_by_file, config):
+            if mutant.tested:
+                progress.register(mutant.cached_status)  # pyright: ignore
+            else:
+                mutants_queue.put(("mutant", mutant.context))  # pyright: ignore
+
     finally:
         for _ in range(NUMBER_OF_PROCESSES_IN_PARALLELIZATION_MODE):
             mutants_queue.put(("end", None))
+
+
+@dataclass
+class Tested:
+    cached_status: StatusResultStr
+    tested: Literal[True] = True
+
+
+@dataclass
+class Untested:
+    context: Context
+    tested: Literal[False] = False
+
+
+def _get_mutants_by_testing_status(
+    mutations_by_file: MutationsByFileReadOnly, config: Config
+) -> Iterator[Tested | Untested]:
+    index = 0
+    for filename, mutations in mutations_by_file.items():
+        cached_mutation_statuses = get_cached_mutation_statuses(
+            filename, mutations, config.hash_of_tests
+        )
+        source = get_source(filename)
+        for mutation_id in mutations:
+            cached_status = cached_mutation_statuses.get(mutation_id)
+            if cached_status is None:
+                raise RuntimeError(f"Cached status not found for {mutation_id}")
+
+            if _is_tested(cached_status):
+                yield Tested(cached_status)
+                continue
+
+            context = Context(
+                mutation_id=mutation_id,
+                filename=filename,
+                dict_synonyms=config.dict_synonyms,
+                config=copy_obj(config),
+                source=source,
+                index=index,
+            )
+            yield Untested(context)
+            index += 1
 
 
 def get_source(filename: FilenameStr) -> str:
@@ -71,5 +98,5 @@ def get_source(filename: FilenameStr) -> str:
     return source
 
 
-def is_tested(status: StatusResultStr) -> bool:
+def _is_tested(status: StatusResultStr) -> bool:
     return status != UNTESTED
