5545bdc5febc4715c52e1f2b96a8c49da030fb57
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-06-06 15:22:44 +0200

    improve log formatting

--- src/tools/setup_logging.py
+++ src/tools/setup_logging.py
@@ -1,10 +1,29 @@
 import atexit
 import logging
 import threading
+import time
 from logging.handlers import MemoryHandler
 from pathlib import Path
 from pprint import pformat
 from typing import Optional
+
+BUFFER_SIZE = 20
+
+
+class CustomFormatter(logging.Formatter):
+    def formatTime(
+        self, record: logging.LogRecord, datefmt: Optional[str] = None
+    ) -> str:
+        # Split date and time
+        created_time = self.converter(record.created)
+        return time.strftime("%H:%M:%S", created_time) + f",{int(record.msecs):03d}"
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.asctime = self.formatTime(record)
+        formatted = super().format(record)
+        if record.message.startswith("\n"):
+            formatted = "\n" + formatted[1:]
+        return formatted
 
 
 class CustomBufferingHandler(MemoryHandler):
@@ -38,18 +57,20 @@ def configure_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
     file_handler.setLevel(level)  # Set the FileHandler level
 
     # Create a formatter and add it to the FileHandler
-    formatter = logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(name)s:%(lineno)d - %(message)s"
+    formatter = CustomFormatter(
+        "%(asctime)s - %(thread)d - %(levelname)s - %(name)s:%(lineno)d - %(message)s"
     )
     file_handler.setFormatter(formatter)
 
     # Add the handlers to the logger
-    buffer_handler = CustomBufferingHandler(20, target=file_handler)
+    buffer_handler = CustomBufferingHandler(BUFFER_SIZE, target=file_handler)
     buffer_handler.setLevel(level)
     logger.addHandler(buffer_handler)
 
-    logger.info("\n")
-    logger.info(f"START (thread id={threading.get_ident()})")
+    created = time.time()
+    created_time = time.localtime(created)
+    date_str = time.strftime("%Y-%m-%d", created_time)
+    logger.info(f"\nSTART (thread id={threading.get_ident()}, date={date_str})")
 
     atexit.register(buffer_handler.flush)
 
